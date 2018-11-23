77811236328c0903d84a9aa58a2b672f9e486380
Klas Bogsjö <42840614lustat@users.noreply.github.com>
2018-11-23 22:59:08 +0100

    read race results into a dataframe

--- loader/read_results.py
+++ loader/read_results.py
@@ -1,81 +1,78 @@
 import requests
 import xml.etree.ElementTree as ET
 import os
+import pandas as pd
 from bs4 import BeautifulSoup
-
-url = "https://eventor.orientering.se/api/results/event"
-apikey = os.environ["apikey"]
-headers = {'ApiKey': apikey}
-#eventid = 18667
-eventid = 23906
-
-response = requests.get(url, headers=headers, params={'eventId': eventid, 'includeSplitTimes': False})
-
-root = ET.fromstringlist(response.text)
-#soup = BeautifulSoup(response.text, 'html.parser')
-#print(soup.prettify())
-#print(root)
-
-# for child in root:
-#     # print('-------------------')
-#     # print(child.tag)
-#     # print(child.attrib)
-#     print(child.text)
-#     for child2 in child:
-#         # print('******************')
-#         # print(child.tag + '/' +child2.tag)
-#         # print(child2.attrib)
-#         for child3 in child2:
-#             # print('3:  ')
-#             print(child.tag + '/' + child2.tag + '/' + child3.tag)
-#             # print(child3.attrib)
-#             # print(child3.text)
-#             for child4 in child3:
-#                 print(child.tag + '/' + child2.tag + '/' + child3.tag+ '/' + child4.tag)
-#                 # print(child4.tag)
-#                 # print(child4.attrib)
-#                 # print(child4.text)
+import numpy as np
 
 
-
-# for racename in root.findall('ClassResult/EventClass/Name'):
-#     print('+++')
-#     print(racename.text)
-#     # personresult.get('Person')
-#     # personresult.get('Result')
-#     # personresult.get('Organisation')
-
-# for x in root.findall('ClassResult/PersonResult/Result/'):
-#     x.get('ResultPosition')
-#     print(str(x.tag) + ': ' + str(x.text))
-#     print('++++ ')
-
-for x in root.findall('ClassResult'):
-    print(x.attrib)
-    obj_eventclass = x.find('EventClass')
-    eventname = obj_eventclass.find('Name').text
-    print(eventname)
-
-    for y in x.findall('PersonResult'):
-        obj_person = y.find('Person')
-        name = obj_person.find('PersonName/Given').text +' ' + obj_person.find('PersonName/Family').text
-        position = obj_person.find('PersonName/Given').text
-
-        obj_org = y.find('Organisation')
-        if obj_org==None:
-            orgid = '?'
-        else:
-            orgid = obj_org.find('OrganisationId').text
+def xmlstring2file(response, xmlname):
+    soup = BeautifulSoup(response.text, 'html.parser')
+    text = soup.prettify()
 
 
-        obj_res = y.find('Result/ResultPosition')
-        if obj_res==None:
-            position = 'x'
-        else:
-            position = obj_res.text
-        print(name + ' ' + position)
-    print('++++ ')
+def get_event(event_id):
+    url = "https://eventor.orientering.se/api/results/event"
+    apikey = os.environ["apikey"]
+    headers = {'ApiKey': apikey}
 
-print('Finished')
+    response = requests.get(url, headers=headers, params={'eventId': eventid, 'includeSplitTimes': False})
+
+    root = ET.fromstringlist(response.text)
+    return root, response
 
 
+def get_resultlist(root):
+    index = 0
+    df = pd.DataFrame()
+    for x in root.findall('ClassResult'):
+        print(x.attrib)
+        obj_eventclass = x.find('EventClass')
+        eventname = obj_eventclass.find('Name').text
+        print()
+        for y in x.findall('PersonResult'):
+            index += 1
+            obj_person = y.find('Person')
+            name = obj_person.find('PersonName/Given').text +' ' + obj_person.find('PersonName/Family').text
+            position = obj_person.find('PersonName/Given').text
+
+            person_id = obj_person.find('PersonId').text
+            obj_birth = obj_person.find('BirthDate/Date')
+            if obj_birth == None:
+                birthyear = np.nan
+            else:
+                birthyear = obj_birth.text[:4]
+
+
+            obj_org = y.find('Organisation')
+            if obj_org == None:
+                orgid = int(0)
+                club = 'Klubblös'
+            else:
+                orgid = int(obj_org.find('OrganisationId').text)
+                club = obj_org.find('Name').text
+
+            obj_res = y.find('Result/ResultPosition')
+            if obj_res == None:
+                position = 'x'
+            else:
+                position = obj_res.text
+
+            df.at[index, 'eventname'] = eventname
+            df.at[index, 'name'] = name
+            df.at[index, 'personid'] = person_id
+            df.at[index, 'birthyear'] = birthyear
+            df.at[index, 'orgid'] = orgid
+            df.at[index, 'club'] = club
+            df.at[index, 'position'] = position
+
+    return df
+
+
+if __name__ == "__main__":
+    # eventid = 18667
+    eventid = 23906
+    xmlroot, resp = get_event(eventid)
+    reslist = get_resultlist(xmlroot)
+    print(reslist)
+    print('Finished')
