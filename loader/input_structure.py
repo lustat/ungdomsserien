4481ca3a174a42ac3c885fc0341b5490eb5485b2
Klas Bogsjö <42840614lustat@users.noreply.github.com>
2019-07-12 11:30:10 +0200

    define expected excel input structure

---
+++ loader/input_structure.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+
+
+def get_input_structure():
+    event_ids = [20550, 21406, 21376, 21988, 21732, 21644]
+    df_parameters = pd.DataFrame(data={'Parameter': ['event_ids', 'night_ids'],
+                                       'Value': ['20550, 21406, 21376, 21988, 21732, 21644', '21851, 21961']})
+
+    dct_parameters = {'example_df': df_parameters,
+                      'compulsory': True,
+                      'description': 'Listar input-parametrar till beräkning (t ex Event ID)'}
+
+    division = list(np.tile(['Elit'], 6))
+    division.extend(list(np.tile(['Division 1'], 7)))
+    division.extend(list(np.tile(['Division 2'], 7)))
+    division.extend(list(np.tile(['Division 3'], 4)))
+    club_id = [483, 639, 125, 258, 471, 114, 35, 614, 54, 487, 167, 169,
+               507, 165, 81, 111, 294, 461, 400, 558, 132, 331, 544, 228]
+
+    club_name = ['Lunds OK', 'Skåneslättens OL', 'Helsingborgs SOK', 'Malmö OK', 'Örkelljunga FK', 'FK Göingarna',
+                 'Andrarums IF', 'Hjärnarps OL', 'FK Boken', 'OK Tyringe', 'Hässleholms OK', 'Hästveda OK',
+                 'OK Kontinent', 'Härlövs IF', 'Frosta OK', 'OK Gynge', 'OK Pan-Kristianstad', 'FK Åsen',
+                 'Tormestorps IF', 'OK Kompassen', 'Eslövs FK', 'IS Skanne', 'Ystads OK', 'IS Kullen']
+
+    df_division = pd.DataFrame(data={'Division': division,
+                                     'Clubid': club_id,
+                                     'Club': club_name})
+
+    dct_division = {'example_df': df_division,
+                    'compulsory': False,
+                    'description': 'Anger divisionstillhörighet for respektive klubb'}
+
+    df_event = pd.DataFrame(data={'Name': ['Förnamn1 Efternamn1', 'Förnamn2 Efternamn2'], 'Class': ['U1', 'Inskolning'],
+                                  'Club': ['Skåneslättens OL', 'Lunds OK']})
+    dct_event = {'example_df': df_event,
+                 'compulsory': False,
+                 'description': 'Anger namn på tävlande med okänd ålder som ska räknas'}
+
+    df_night = pd.DataFrame(data={'Person ID': ['11111', '222222'],
+                                  'Name': ['Förnamn1 Efternamn1', 'Förnamn2 Efternamn2'],
+                                  'Club': ['Skåneslättens OL', 'Lunds OK'],
+                                  'U-series': ['D12', 'H14'],
+                                  'Night-class': ['U2', 'Öppen Motion 5'],
+                                  'Event ID': [21930, 22921]})
+    dct_night = {'example_df': df_night,
+                 'compulsory': False,
+                 'description': 'Anger deltagande i natt-tävling, i natt-tävlingar som inte listats via "night_ids" i "Parameters"-flik.'}
+
+
+    structure = {'Parameters': dct_parameters,
+                 'Division': dct_division,
+                 str(event_ids[0]): dct_event,
+                 'Night': dct_night}
+    return structure
+
+
+def create_excel_template(structure):
+    # TODO create excel template here
+    return None
+
+if __name__ == '__main__':
+    template = get_input_structure()
+    file = create_excel_template(template
+
+    print('Finished')
