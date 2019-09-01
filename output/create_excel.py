47f3649d42696c455834e64ee5450999cb57d2aa
Klas Bogsjö <42840614lustat@users.noreply.github.com>
2019-09-01 18:05:43 +0200

    add green red color in club division summary

--- output/create_excel.py
+++ output/create_excel.py
@@ -5,6 +5,7 @@ import openpyxl
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.styles import Font, Color
 from openpyxl.styles.borders import Border, Side
+from openpyxl.styles import PatternFill
 
 
 def individual_results_excel(storage_path, dct):
@@ -72,21 +73,38 @@ def club_results_to_excel(storage_path, df, club_results):
         for (key, row) in data.iterrows():
             division_data = data.loc[data.division == row.division]
             data.at[key, 'position'] = 1 + sum(row.score < division_data.score)
-            data.at[key, 'negative_position'] = sum(row.score > division_data.score)
+            data.at[key, 'negative_position'] = 1 + sum(row.score > division_data.score)
 
         data = data.assign(line_above=False)
+        data = data.assign(red_place=False)
+        data = data.assign(green_place=False)
+
         for division in data.division.unique():
             division_data = data.loc[data.division == division]
             if len(division_data) >= 4:
+                first_or_2nd = division_data.loc[(division_data.position == 1) |
+                                                 (division_data.position == 2)]
+                if len(first_or_2nd) == 2:
+                    if division.lower() != 'elit':
+                        data.at[first_or_2nd.index[0], 'green_place'] = True
+                        data.at[first_or_2nd.index[1], 'green_place'] = True
+
                 third_position = division_data.loc[division_data.position == 3]
                 if len(third_position) == 1:
                     if division.lower() != 'elit':
                         data.at[third_position.index[0], 'line_above'] = True
 
-                second_last = division_data.loc[division_data.negative_position == 1]
+                second_last = division_data.loc[division_data.negative_position == 2]
                 if len(second_last) == 1:
                     if division.lower() != 'division 3':
                         data.at[second_last.index[0], 'line_above'] = True
+
+                last_or_2nd_last = division_data.loc[(division_data.negative_position == 1) |
+                                                     (division_data.negative_position == 2)]
+                if len(last_or_2nd_last) == 2:
+                    if division.lower() != 'division 3':
+                        data.at[last_or_2nd_last.index[0], 'red_place'] = True
+                        data.at[last_or_2nd_last.index[1], 'red_place'] = True
 
         data = data.drop(columns={'position', 'negative_position'})
         return data
@@ -95,16 +113,26 @@ def club_results_to_excel(storage_path, df, club_results):
         previous_division = ''
         last_row_header = True
         for row in dataframe_to_rows(data, index=False, header=True):
-            current_division = row[-2]
-            line_above = row[-1]
-            print_row = row[:-2]
+            current_division = row[-4]
+            line_above = row[-3]
+            red_place = row[-2]
+            green_place = row[-1]
+            print_row = row[:-4]
             if previous_division:
                 if current_division == previous_division:
                     worksheet.append(print_row)
-                    if line_above:
+                    # if line_above:
+                    #     for column_number in range(1, len(print_row) + 1):
+                    #         cell = worksheet.cell(worksheet.max_row, column_number)
+                    #         cell.border = Border(top=Side(style='dotted'))
+                    if red_place:
                         for column_number in range(1, len(print_row) + 1):
                             cell = worksheet.cell(worksheet.max_row, column_number)
-                            cell.border = Border(top=Side(style='dotted'))
+                            cell.fill = PatternFill('lightTrellis', fgColor=Color('55FF8888'))
+                    if green_place:
+                        for column_number in range(1, len(print_row) + 1):
+                            cell = worksheet.cell(worksheet.max_row, column_number)
+                            cell.fill = PatternFill('lightTrellis', fgColor=Color('5588FF88'))
                 else:
                     empty_row = [' ' for str in row]
                     if last_row_header:
@@ -119,6 +147,10 @@ def club_results_to_excel(storage_path, df, club_results):
                     cell = worksheet.cell(worksheet.max_row, 1)
                     cell.font = Font(bold=True)
                     worksheet.append(print_row)
+                    if green_place:
+                        for column_number in range(1, len(print_row) + 1):
+                            cell = worksheet.cell(worksheet.max_row, column_number)
+                            cell.fill = PatternFill('lightTrellis', fgColor=Color('5588FF88'))
             else:
                 worksheet.append(print_row)
             previous_division = current_division
