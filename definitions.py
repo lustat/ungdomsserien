ea668f4a4166e2e060b37c5e93f49b8e8c870b14
Klas Bogsjö <42840614lustat@users.noreply.github.com>
2022-03-27 16:18:55 +0200

    set event count to 4

--- definitions.py
+++ definitions.py
@@ -3,7 +3,7 @@ import os
 SOURCE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = SOURCE_DIR + '/data'
 
-EVENT_COUNT = 3  # Number of events to be counted
+EVENT_COUNT = 4  # Number of events to be counted
 
 
 
