6f9661ae37d9d7425114d9499a204e83c2b6b4e3
Klas Bogsjö <42840614lustat@users.noreply.github.com>
2022-03-27 16:17:36 +0200

    cahnge input to 2022

--- calculation/summarize.py
+++ calculation/summarize.py
@@ -133,7 +133,7 @@ def club_summary(df, division_df, event_column='event_date'):
                     print('Klubbnamn i divisionstabell matchar ej')
                     print('Dubbelkolla Excel-fil')
                     for (key, row) in mismatch.iterrows():
-                        print(row.club + '  =?   ' + row.club_division)
+                        print(str(row.club) + '  =?   ' + str(row.club_division))
                     print(' ')
                 summary = summary.drop(columns=['club_division'])
         else:
