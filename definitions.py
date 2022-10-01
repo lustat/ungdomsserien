adae0a3f27bde35a8d44a78949bb4a9e45b0565c
Klas Bogsjö <klasbogsjo@gmail.com>
2022-10-01 12:18:56 +0200

    prepare algots calcs

--- definitions.py
+++ definitions.py
@@ -2,6 +2,7 @@ import os
 
 SOURCE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = SOURCE_DIR + '/data'
+OUTPUT_DIR = SOURCE_DIR + '/output'
 
 EVENT_COUNT = 4  # Number of events to be counted
 
