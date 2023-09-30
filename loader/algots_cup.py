28ba28623534c730ba3a3f452bf86f1ccafb4b2a
Klas Bogsjö <klasbogsjo@gmail.com>
2023-09-30 14:13:49 +0200

    update with algots 2023

--- loader/algots_cup.py
+++ loader/algots_cup.py
@@ -9,17 +9,18 @@ from loader.read_results import extract_and_analyse
 if __name__ == "__main__":
     print('Älgots Cup 2021: 32730)')
     print('Älgots Cup 2022: 34537)')
+    print('Älgots Cup 2023: 46558)')
     storage_path = f'{OUTPUT_DIR}/algots_cup_{datetime.datetime.now().year}'
 
     use_manual_file = True
     if use_manual_file:
-        manual, club_division, user_dct = read_manual_input(manual_input_file='C:/PycharmProjects/ungdomsserien/input/Manual-input-2022-algots.xlsx')
+        manual, club_division, user_dct = read_manual_input(manual_input_file='C:/PycharmProjects/ungdomsserien/input/Manual-input-2023-algots.xlsx')
     else:
         manual = pd.DataFrame()
 
         os.makedirs(storage_path, exist_ok=True)
         club_division = pd.DataFrame()
-        user_dct = {'event_ids': '32730',
+        user_dct = {'event_ids': '46558',
                     'night_ids': ''}
 
     extract_and_analyse(storage_path=storage_path, race_to_manual_info=manual,
