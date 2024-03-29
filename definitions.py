2c010a18ad598aed202ee7d2c921d8376e3e674a
Klas Bogsjö <klasbogsjo@gmail.com>
2024-03-29 20:15:00 +0100

    draft code

--- definitions.py
+++ definitions.py
@@ -4,6 +4,7 @@ SOURCE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = SOURCE_DIR + '/data'
 OUTPUT_DIR = SOURCE_DIR + '/output'
 
+NOT_STARTED_NAMES = {'didnotstart', 'cancelled'}
 EVENT_COUNT = 4  # Number of events to be counted
 
 
