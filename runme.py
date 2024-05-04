c21323d712904f29b46750a29ccaa217dc3bdd31
Klas Bogsjö <klasbogsjo@gmail.com>
2024-05-04 16:37:27 +0200

    update file names

--- runme.py
+++ runme.py
@@ -1,7 +1,5 @@
 from dotenv import load_dotenv
 import pandas as pd
-import os
-
 from loader.read_manual_excel import read_manual_input
 from loader.read_results import extract_and_analyse
 from definitions import DATA_DIR
@@ -17,22 +15,21 @@ if __name__ == "__main__":
                             club_division_df=club_division,
                             user_input=user_dct)
     else:
-        print('Älgots Cup 2021: 32730)')
-        print('Älgots Cup 2022: 34537)')
-        print('Älgots Cup 2023: 46558)')
-        storage_path = f'{OUTPUT_DIR}/algots_cup_{datetime.datetime.now().year}'
+        # print('Älgots Cup 2021: 32730)')
+        # print('Älgots Cup 2022: 34537)')
+        # print('Älgots Cup 2023: 46558)')
 
         use_manual_file = True
         if use_manual_file:
             manual, club_division, user_dct = read_manual_input(
-                manual_input_file='/data/01_input/Manual-input-2023-algots.xlsx')
+                manual_input_file=f'{DATA_DIR}/01_input/Manual-input-2023-algots.xlsx')
         else:
             manual = pd.DataFrame()
-
-            os.makedirs(storage_path, exist_ok=True)
             club_division = pd.DataFrame()
             user_dct = {'event_ids': '46558',
                         'night_ids': ''}
 
-        extract_and_analyse(storage_path=storage_path, race_to_manual_info=manual,
-                            club_division_df=club_division, user_input=user_dct)
+        extract_and_analyse(race_to_manual_info=manual,
+                            club_division_df=club_division,
+                            user_input=user_dct,
+                            algots_cup=True)
