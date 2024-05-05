af2e7485e249d797e84d61f0e9ec370ef17edb91
Klas Bogsjö <klasbogsjo@gmail.com>
2024-05-05 12:20:41 +0200

    one shot yaml update

--- sandbox/create_organisation_yaml_file.py
+++ sandbox/create_organisation_yaml_file.py
@@ -1,22 +1,28 @@
-# Create dictionary of organisation ID
 import os
 import pandas as pd
 import yaml
 from definitions import DATA_DIR
 
 
-event_files = f'{DATA_DIR}/02_raw_data'
-files = [filename for filename in os.listdir(event_files) if filename.startswith('event_')]
+def create_yaml_file():
+    """ This creates a yaml file for translation from club-id to orgainsation-id- The orgainsation-id is
+    used to single out runners from Skåne"""
+    event_files = f'{DATA_DIR}/02_raw_data'
+    files = [filename for filename in os.listdir(event_files) if filename.startswith('event_')]
 
-dfs = []
-for file in files:
-    df0 = pd.read_parquet(f'{DATA_DIR}/02_raw_data/{file}')
-    dfs.append(df0)
-df = pd.concat(dfs, axis=0)
-df = df[['orgid', 'region']].drop_duplicates()
-df = df.set_index('orgid')
-club_to_org = df.to_dict()['region']
+    dfs = []
+    for file in files:
+        df0 = pd.read_parquet(f'{DATA_DIR}/02_raw_data/{file}')
+        dfs.append(df0)
+    df = pd.concat(dfs, axis=0)
+    df = df[['orgid', 'region']].drop_duplicates()
+    df = df.set_index('orgid')
+    club_to_org = df.to_dict()['region']
 
-with open(f"{DATA_DIR}/02_raw_data/club_to_region.yaml", "w") as file:
-    yaml.dump(club_to_org, file)
-print("YAML file saved.")
+    with open(f"{DATA_DIR}/02_raw_data/club_to_region.yaml", "w") as file:
+        yaml.dump(club_to_org, file)
+    print("YAML file saved.")
+
+
+if __name__ == '__main__':
+    create_yaml_file()
